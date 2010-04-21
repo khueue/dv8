6809d602c04f1c872f2f2b784a64fc84774a4689
selu7901 <selu7901@student.uu.se>
2010-04-21 15:04:37 +0200

    New tty files.

---
+++ src/tty.c
@@ -0,0 +1,27 @@
+#include "tty.h"
+
+static volatile tty_t* const tty = (tty_t*) 0xb80003f8;
+
+void tty_putc(char c) {
+  // Poll until ready to transmit.
+  while ( !tty->lsr.thre ) {}		
+
+  // Write character to Transmitter Holding Register
+  tty->thr = c;		    
+	  
+}
+
+void tty_print_string(const char* text) {
+  while (text[0] != '\0') {
+    tty_putc(text[0]);
+    ++text;
+  }
+}
+
+void tty_test() {
+  tty_putc('A');
+
+  tty_print_string("loha!");
+
+  while (1) {};
+}
