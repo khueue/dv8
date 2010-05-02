95e93282f6c1530fdc7d233d9bac3883c61e8ccc
selu7901 <selu7901@student.uu.se>
2010-05-02 14:44:42 +0200

    Cosmetics.

--- include/asm.h
+++ include/asm.h
@@ -9,13 +9,18 @@ static const int timer_usec = 67;
 static const int timer_msec = 67000;
 
 /* Functions provided by 'asm.S' */
+
 uint32_t kset_sr(uint32_t and_mask, uint32_t or_mask);
 uint32_t kget_sr(void);
+
 uint32_t kset_cause(uint32_t and_mask, uint32_t or_mask);
 uint32_t kget_cause(void);
+
 void kload_timer(uint32_t timer_value);
+
 void kset_registers(registers_t* regs);
 registers_t* kget_registers(void);
+
 void kdebug_magic_break(void);
 
 /*
