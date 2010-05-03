b47fb30ccecfc1e676f770abe482af328204b2c0
selu7901 <selu7901@student.uu.se>
2010-05-03 17:12:19 +0200

    int length -> size_t

--- include/prio_queue.h
+++ include/prio_queue.h
@@ -1,5 +1,5 @@
-#ifndef LIFO_QUEUE_H
-#define LIFO_QUEUE_H
+#ifndef PRIO_QUEUE_H
+#define PRIO_QUEUE_H
 
 #include "list_node.h"
 
@@ -12,14 +12,13 @@
 /*
  * Priority queue struct where cmp_fun is the compare function.
  */
-struct prio_queue;
 typedef struct prio_queue prio_queue_t;
 struct prio_queue
 {
     list_node_t *head;
     list_node_t *foot;
     int (*cmp_fun)(void *, void *);
-    int length;
+    size_t length;
 };
 
 /*
