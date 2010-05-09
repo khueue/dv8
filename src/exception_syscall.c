cb1c20e07da539229553b6eeb68dc8d7dbc5cf4f
selu7901 <selu7901@student.uu.se>
2010-05-09 21:51:38 +0200

    Added comment.

--- src/exception_syscall.c
+++ src/exception_syscall.c
@@ -23,11 +23,12 @@ execute_syscall(cause_reg_t cause)
         uint32_t ret = 0;
         
         /* Get pointer to stored registers. */
-        registers_t* regs = kget_registers();
+        registers_t *regs = kget_registers();
 
         /* Handle the system call (see kernel_api.S). */
         ret = ksyscall_handler(regs);
 
+        /* Save return value of syscall function. */
         regs->v_reg[0] = ret;
 
         /* Return from exception to instruction following syscall. */
