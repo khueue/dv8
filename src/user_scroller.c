1314c4e4ab4adeaa28502aafd96c758b7dad8c50
dahe9761 <david.hesselbom@gmail.com>
2010-05-10 13:08:48 +0200

    Changed scroller message

--- src/user_scroller.c
+++ src/user_scroller.c
@@ -19,18 +19,7 @@ maltascr(void)
 
     /* Funkar för alla strängar utom något som BÖRJAR med null */
 
-    uint8_t tomalta[11];
-    tomalta[0] = 'b';
-    tomalta[1] = 'a';
-    tomalta[2] = 'l';
-    tomalta[3] = 'l';
-    tomalta[4] = 'e';
-    tomalta[5] = 'f';
-    tomalta[6] = 'j';
-    tomalta[7] = 'o';
-    tomalta[8] = 'n';
-    tomalta[9] = 'g';
-    tomalta[10] = 0;
+    uint8_t tomalta[] = "Kernel Panic ***";
 
     malta->ledbar.reg = 0xFF;
 
