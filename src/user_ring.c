65eb14a0c92a486f6717b18e306bf0962dad9cb4
dahe9761 <david.hesselbom@gmail.com>
2010-05-12 16:26:17 +0200

    pillat som fan

--- src/user_ring.c
+++ src/user_ring.c
@@ -25,16 +25,18 @@ ring(int number_of_nodes)
     int pids[number_of_nodes]; 
     
     int i = 0;
-    pids[i] = exec(ringnode);
+    pids[i] = exec(ringnode, nån_prio);
     
     for (i = 1; i < number_of_nodes; i++)
     {
-        pids[i] = exec(ringnode);
-        send_int_message(pids[i-1], pids[i]);
+        pids[i] = exec(ringnode, nån_prio);
+        send_int_message(pids[i-1], pids[i]); // skicka pids[i] till pids[i-1]
     }
     
-    send_int_message(pids[number_of_nodes], pids[0]);
+    send_int_message(pids[number_of_nodes], pids[0]); // skicka första nodens pid till sista noden
     
-    send_string_message_and_awaken(pids[0], "hej hopp");
+    send_string_message_and_awaken(pids[0], "hej hopp"); // sätt igång ringen
+    
+    // dö
     */
 }
