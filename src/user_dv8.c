550dc7f36b33297885142ea5040a0eb99af4cbd1
jobo0809 <jobo0809@student.uu.se>
2010-05-20 20:21:03 +0200

    Dv8! panda penis.

---
+++ src/user_dv8.c
@@ -0,0 +1,20 @@
+#include "utils.h"
+#include "kernel_api.h"
+#include "msg.h"
+
+#include "user_dv8.h"
+#include "user_dv8.h"
+
+void
+dv8(void)
+{
+    print_strln("");
+    print_strln("            888888ba                    oo            dP");
+    print_strln("            88    `8b                                 88");
+    print_strln("            88     88 .d8888b. dP   .dP dP .d8888b. d8888P .d8888b.");
+    print_strln("            88     88 88ooood8 88   d8' 88 88'  `88   88   88ooood8");
+    print_strln("            88    .8P 88.  ... 88 .88'  88 88.  .88   88   88.  ...");
+    print_strln("            8888888P  `88888P' 8888P'   dP `88888P8   dP   `88888P'");
+    print_strln("            ooooooooooooooooooooooooooooooooooo GROUP DV8 2010 oooo");
+    print_strln("");
+}
