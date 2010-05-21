8f823645d185f6a5efe94b91db417b48441db92e
selu7901 <selu7901@student.uu.se>
2010-05-21 13:41:58 +0200

    Newlines, I think.

--- src/user_man.c
+++ src/user_man.c
@@ -29,17 +29,17 @@ man(void)
 {
     msg_t msg_struct;
     msg_t *msg = &msg_struct;
-    char arg[256];    
-    
-    read_message_by_type(msg, MSG_TYPE_ARGUMENT, 330);    
+    char arg[256];
+
+    read_message_by_type(msg, MSG_TYPE_ARGUMENT, 330);
     if (msg_type_is_invalid(msg))
     {
         jon_old_as_hell();
         return;
     }
-    
+
     strcpy(arg,msg_data_get_string(msg));
-    
+
     if (0 == (strcmp("fib", arg)))
     {
         print_strln("Fib workes like this");
