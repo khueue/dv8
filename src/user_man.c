8296f4d64ef49a3db4df129c500b7231d83ab960
siun1641 <simon.unge.1641@student.uu.se>
2010-05-21 09:15:32 +0200

    Added man, workes ölike a charm.

---
+++ src/user_man.c
@@ -0,0 +1,25 @@
+#include "utils.h"
+#include "kernel_api.h"
+#include "msg.h"
+
+#include "user_man.h"
+
+void
+man(void)
+{
+    print_strln("");
+    print_strln("  _|_|_|  _|_|_|      _|_|    _|_|_|_|_|  _|_|_|_|_|  _|_|_|    _|_|_| "); 
+    print_strln("_|        _|    _|  _|    _|      _|          _|        _|    _|       "); 
+    print_strln("_|  _|_|  _|_|_|    _|_|_|_|      _|          _|        _|      _|_|   "); 
+    print_strln("_|    _|  _|    _|  _|    _|      _|          _|        _|          _| "); 
+    print_strln("  _|_|_|  _|    _|  _|    _|      _|          _|      _|_|_|  _|_|_|   "); 
+    print_strln("                                                                       ");  
+    print_strln("                                                                       ");  
+    print_strln("                                                                       ");
+    print_strln("                        _|    _|_|    _|      _|  _|                   ");
+    print_strln("                        _|  _|    _|  _|_|    _|  _|                   ");
+    print_strln("                        _|  _|    _|  _|  _|  _|  _|                   ");
+    print_strln("                  _|    _|  _|    _|  _|    _|_|                       ");
+    print_strln("                    _|_|      _|_|    _|      _|  _|                   ");
+    print_strln("");
+}
