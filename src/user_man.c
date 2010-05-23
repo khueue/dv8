4769fdd0584b7522c515f3eeb74211fd2388f9fa
siun1641 <simon.unge.1641@student.uu.se>
2010-05-23 12:21:04 +0200

    Bahf

--- src/user_man.c
+++ src/user_man.c
@@ -29,29 +29,17 @@ man(void)
 {
     msg_t msg_struct;
     msg_t *msg = &msg_struct;
-<<<<<<< HEAD:src/user_man.c
-    char arg[256];    
-    
-    read_message_by_type(msg, MSG_TYPE_ARGUMENT, 330);    
-=======
     char arg[256];
 
     read_message_by_type(msg, MSG_TYPE_ARGUMENT, 330);
->>>>>>> 8f823645d185f6a5efe94b91db417b48441db92e:src/user_man.c
     if (msg_type_is_invalid(msg))
     {
         jon_old_as_hell();
         return;
     }
-<<<<<<< HEAD:src/user_man.c
-    
-    strcpy(arg,msg_data_get_string(msg));
-    
-=======
 
     strcpy(arg,msg_data_get_string(msg));
 
->>>>>>> 8f823645d185f6a5efe94b91db417b48441db92e:src/user_man.c
     if (0 == (strcmp("fib", arg)))
     {
         print_strln("Fib works like this");
